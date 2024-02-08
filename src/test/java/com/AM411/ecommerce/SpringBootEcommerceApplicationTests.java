744c5139062ab33182eac0c5268a5dd3afab6468
Almir Motika <Almir_18B@hotmail.com>
2024-02-08 17:09:37 +0100

    Create Spring Boot Starter Project

---
+++ src/test/java/com/AM411/ecommerce/SpringBootEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AM411.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
