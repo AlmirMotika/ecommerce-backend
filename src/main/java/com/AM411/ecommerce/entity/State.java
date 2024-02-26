2661a6b21eb30f83128af4cf3d3d21413a90287c
Almir Motika <Almir_18B@hotmail.com>
2024-02-26 15:51:25 +0100

    8. develop jpa entities(country,state)

---
+++ src/main/java/com/AM411/ecommerce/entity/State.java
@@ -0,0 +1,20 @@
+package com.AM411.ecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "state")
+@Data
+public class State {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "name")
+    private String name;
+    @ManyToOne
+    @JoinColumn(name="country_id")
+    private Country country;
+}
